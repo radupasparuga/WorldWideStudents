9d7fbd14da139ead7849e02ed4fedc08f6c8f823
radupasparuga <radupasparuga25@gmail.com>
2019-03-18 18:13:49 +0200

    Remade home page to be dashboard when logged

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -1,13 +1,46 @@
 /* eslint-disable */
 import React,{ Component } from 'react'
+import { Link } from 'react-router-dom'
+import PropTypes from "prop-types";
+import { connect } from "react-redux";
+import { withRouter } from "react-router-dom";
+import Dashboard from "./Dashboard";
 /* eslint-enable */
 
-export default class Home extends Component {
+class Home extends Component {
   render() {
+    const { isAuthenticated } = this.props.auth;
+    const loggedIn = (
+      <Dashboard />
+    )
+    const guest = (
+      <div>
+        <ul className="navbar-nav ml-auto">
+          <li className="nav-item">
+            <Link className="nav-link" to="/register">Sign Up</Link>
+          </li>
+          <li className="nav-item">
+            <Link className="nav-link" to="/login">Sign In</Link>
+          </li>
+        </ul>
+      </div>
+    )
     return (
       <div>
-        Home Component
+        {isAuthenticated ? loggedIn : guest}
       </div>
     )
   }
 }
+
+Home.propTypes = {
+  auth: PropTypes.object.isRequired
+};
+
+const mapStateToProps = state => ({
+  auth: state.auth
+});
+
+export default connect(
+  mapStateToProps
+  )(withRouter(Home));
--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -1,65 +1,78 @@
-import React, { Component } from 'react'
-import { Link } from 'react-router-dom'
-import PropTypes from 'prop-types'
-import { connect } from 'react-redux'
-import { logoutUser } from '../actions/authentication'
-import { withRouter } from 'react-router-dom'
+import React, { Component } from "react";
+import { Link } from "react-router-dom";
+import PropTypes from "prop-types";
+import { connect } from "react-redux";
+import { logoutUser } from "../actions/authentication";
+import { withRouter } from "react-router-dom";
 
 class Navbar extends Component {
-
   onLogout(e) {
-    e.preventDefault()
-    this.props.logoutUser(this.props.history)
+    e.preventDefault();
+    this.props.logoutUser(this.props.history);
   }
 
   render() {
-    const {isAuthenticated} = this.props.auth
+    const { isAuthenticated } = this.props.auth;
     const authLinks = (
-      <ul className="navbar-nav ml-auto">
-        <li className="nav-item">
-          <Link className="nav-link" to="/dashboard">Dashboard</Link>
-        </li>
-        <li className="nav-item">
-          <Link className="nav-link" to="/meet">Meet & Greet</Link>
-        </li>
-        <li className="nav-item">
-          <Link className="nav-link" to="/profile">Profile</Link>
-        </li>
-        <a href="" className="nav-link" onClick={this.onLogout.bind(this)}>
-                    Logout
-        </a>
-      </ul>
-    )
-    const guestLinks = (
-      <ul className="navbar-nav ml-auto">
-        <li className="nav-item">
-          <Link className="nav-link" to="/register">Sign Up</Link>
-        </li>
-        <li className="nav-item">
-          <Link className="nav-link" to="/login">Sign In</Link>
-        </li>
-      </ul>
-    )
-    return(
       <nav className="navbar navbar-expand-lg  navbar-light bg-light">
-        <Link className="navbar-brand" to="/">WorldWide Students</Link>
+        <Link className="navbar-brand" to="/">
+          World Wide Students
+        </Link>
         <div className="collapse navbar-collapse" id="navbarSupportedContent">
-          {isAuthenticated ? authLinks : guestLinks}
+          <ul className="navbar-nav ml-auto">
+            <li className="nav-item">
+              <Link className="nav-link" to="/">
+                Dashboard
+              </Link>
+            </li>
+            <li className="nav-item">
+              <Link className="nav-link" to="/meet">
+                Meet & Greet
+              </Link>
+            </li>
+            <li className="nav-item">
+              <Link className="nav-link" to="/profile">
+                Profile
+              </Link>
+            </li>
+            <a href="" className="nav-link" onClick={this.onLogout.bind(this)}>
+              Logout
+            </a>
+          </ul>
         </div>
-        <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-          <span className="navbar-toggler-icon"></span>
+        <button
+          className="navbar-toggler"
+          type="button"
+          data-toggle="collapse"
+          data-target="#navbarSupportedContent"
+          aria-controls="navbarSupportedContent"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span className="navbar-toggler-icon" />
         </button>
       </nav>
-    )
+    );
+    const guestLinks = (
+      <nav className="navbar navbar-expand-lg  navbar-light bg-light">
+        <Link className="navbar-brand" to="/">
+          World Wide Students
+        </Link>
+      </nav>
+    );
+    return <div>{isAuthenticated ? authLinks : guestLinks}</div>;
   }
 }
 Navbar.propTypes = {
   logoutUser: PropTypes.func.isRequired,
   auth: PropTypes.object.isRequired
-}
+};
 
-const mapStateToProps = (state) => ({
+const mapStateToProps = state => ({
   auth: state.auth
-})
+});
 
-export default connect(mapStateToProps, { logoutUser })(withRouter(Navbar))
+export default connect(
+  mapStateToProps,
+  { logoutUser }
+)(withRouter(Navbar));
